01ff1784d08dc478f7e8e1cd876daadf1846203b
Armand <armand2ngassu@gmail.com>
2018-02-07 23:00:56 +0100

    Config database access with hibernate and jpa

--- src/main/java/com/springmvc/gnote/HomeController.java
+++ src/main/java/com/springmvc/gnote/HomeController.java
@@ -6,10 +6,18 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import entities.Users;
+import services.UserService;
 
 /**
  * Handles requests for the application home page.
@@ -22,6 +30,15 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	private UserService userService;
+	
+	@Autowired(required=true)
+	@Qualifier(value="userService")
+	public void setUserService(UserService us) {
+		this.userService = us;
+	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -30,6 +47,17 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
+		
+		/*Test*/
+		Users u = new Users();
+		u.setEmail("armandnjapou@gmail.com");
+		u.setLogin("anjapou");
+		u.setPassword("password1234");
+		userService.addUser(u);
+		System.out.println("Record done succesfully !!!");
+		u.setEmail("njapsolo@yahoo.fr");
+		userService.updateUser(u);
+		System.out.println("Update succeed !!!");
 		
 		model.addAttribute("serverTime", formattedDate );
 		
